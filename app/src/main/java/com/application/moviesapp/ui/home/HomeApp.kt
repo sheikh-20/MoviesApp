a9220a73f3985726d5681e045db9b47058870290
sheikh-10 <mohammedsheikhmohideen2002@gmail.com>
2023-08-18 19:00:34 +0530

    Added condition on specific screen title

--- app/src/main/java/com/application/moviesapp/ui/home/HomeApp.kt
+++ app/src/main/java/com/application/moviesapp/ui/home/HomeApp.kt
@@ -2,9 +2,14 @@ package com.application.moviesapp.ui.home
 
 import android.app.Activity
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.FileDownload
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Tune
 import androidx.compose.material.icons.rounded.Bookmark
 import androidx.compose.material.icons.rounded.Explore
 import androidx.compose.material.icons.rounded.Home
@@ -12,11 +17,14 @@ import androidx.compose.material.icons.rounded.NotificationsNone
 import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -30,6 +38,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -51,7 +60,7 @@ fun HomeApp(modifier: Modifier = Modifier, navController: NavHostController = re
     val uiState: MoviesWithNewReleaseUiState by homeViewModel.moviesWithNewReleaseUiState.collectAsState()
 
     Scaffold(
-        topBar = { HomeTopAppbar() },
+        topBar = { HomeTopAppbar(navController) },
         bottomBar = { HomeBottomBarNavigation(navController) },
         containerColor = Color.Transparent
     ) { paddingValues ->
@@ -78,30 +87,109 @@ fun HomeApp(modifier: Modifier = Modifier, navController: NavHostController = re
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun HomeTopAppbar() {
+private fun HomeTopAppbar(navController: NavHostController) {
 
     val context = LocalContext.current
 
-    TopAppBar(
-        title = {},
-        navigationIcon = {
-            IconButton(onClick = {   }) {
-                Icon(painter = painterResource(id = R.drawable.ic_movie),
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.onSecondary)
-            }
-        },
-        actions = {
-            IconButton(onClick = {}) {
-                Icon(imageVector = Icons.Rounded.Search, contentDescription = null)
-            }
+    when (navController.currentBackStackEntryAsState().value?.destination?.route) {
+        BottomNavigationScreens.Explore.route -> {
+            TopAppBar(
+                title = {
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {}, 
+                        label = {  
+                            Text(text = "Search")
+                        },
+                        leadingIcon = {
+                            Icon(imageVector = Icons.Rounded.Search, contentDescription = null)
+                        },
+                        modifier = Modifier
+                            .height(64.dp)
+                            .fillMaxWidth()
+                            .padding(end = 20.dp),
+                        shape = RoundedCornerShape(30)
+                        )
+                        },
+                actions = {
+                    FloatingActionButton(onClick = {  }, elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(defaultElevation = 0.dp)) {
+                        Icon(imageVector = Icons.Outlined.Tune, contentDescription = null)
+                    }
+                },
+            )
+        }
+        BottomNavigationScreens.MyList.route -> {
+            TopAppBar(
+                title = { Text(text = "My List") },
+                navigationIcon = {
+                    IconButton(onClick = {   }) {
+                        Icon(painter = painterResource(id = R.drawable.ic_movie),
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onSecondary)
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Rounded.Search, contentDescription = null)
+                    }
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Transparent)
+            )
+        }
+        BottomNavigationScreens.Download.route -> {
+            TopAppBar(
+                title = { Text(text = "Download") },
+                navigationIcon = {
+                    IconButton(onClick = {   }) {
+                        Icon(painter = painterResource(id = R.drawable.ic_movie),
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onSecondary)
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Rounded.Search, contentDescription = null)
+                    }
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Transparent)
+            )
+        }
+        BottomNavigationScreens.Profile.route -> {
+            TopAppBar(
+                title = { Text(text = "Profile") },
+                navigationIcon = {
+                    IconButton(onClick = {   }) {
+                        Icon(painter = painterResource(id = R.drawable.ic_movie),
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onSecondary)
+                    }
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Transparent)
+            )
+        }
+        else -> {
+            TopAppBar(
+                title = {},
+                navigationIcon = {
+                    IconButton(onClick = {   }) {
+                        Icon(painter = painterResource(id = R.drawable.ic_movie),
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onSecondary)
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Rounded.Search, contentDescription = null)
+                    }
 
-            IconButton(onClick = { NotificationActivity.startActivity(context as Activity) }) {
-                Icon(imageVector = Icons.Rounded.NotificationsNone, contentDescription = null)
-            }
-        },
-        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Transparent)
-    )
+                    IconButton(onClick = { NotificationActivity.startActivity(context as Activity) }) {
+                        Icon(imageVector = Icons.Rounded.NotificationsNone, contentDescription = null)
+                    }
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Transparent)
+            )
+        }
+    }
 }
 
 @Composable
