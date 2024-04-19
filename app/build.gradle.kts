4b06d7e50cdddfc56dbd54cfee4fd922f6dd164c
sheikh-20 <sheikhzs1032@gmail.com>
2024-04-19 16:37:19 +0530

    build gradle version increment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ android {
         applicationId = "com.application.moviesapp"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
