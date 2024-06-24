f2717abc889ce3b1869d63732e168692f5d060bf
sheikh-20 <sheikhzs1032@gmail.com>
2024-06-24 10:41:13 +0530

    tv series review api integrated

--- app/src/main/java/com/application/moviesapp/data/api/response/UserReviewDto.kt
+++ app/src/main/java/com/application/moviesapp/data/api/response/UserReviewDto.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class MovieReviewDto(
+data class UserReviewDto(
 
     @SerialName("id")
     val id: Int?,
--- app/src/main/java/com/application/moviesapp/data/remote/TvSeriesPagingSource.kt
+++ app/src/main/java/com/application/moviesapp/data/remote/TvSeriesPagingSource.kt
@@ -3,24 +3,23 @@ package com.application.moviesapp.data.remote
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.application.moviesapp.data.api.MoviesApi
-import com.application.moviesapp.data.api.response.MovieReviewDto
-import com.application.moviesapp.data.api.response.TvSeriesReviewDto
+import com.application.moviesapp.data.api.response.UserReviewDto
 import timber.log.Timber
 
-class TvSeriesReviewPagingSource(private val moviesApi: MoviesApi, private val seriesId: Int): PagingSource<Int, TvSeriesReviewDto.Result>() {
+class TvSeriesReviewPagingSource(private val moviesApi: MoviesApi, private val seriesId: Int): PagingSource<Int, UserReviewDto.Result>() {
 
     private companion object {
         const val TAG = "TvSeriesReviewPagingSource"
     }
 
-    override fun getRefreshKey(state: PagingState<Int, TvSeriesReviewDto.Result>): Int? {
+    override fun getRefreshKey(state: PagingState<Int, UserReviewDto.Result>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
                 ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
         }
     }
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, TvSeriesReviewDto.Result> {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, UserReviewDto.Result> {
 
         Timber.tag(TAG).d("%s Called!", TAG)
 
@@ -37,7 +36,7 @@ class TvSeriesReviewPagingSource(private val moviesApi: MoviesApi, private val s
             }
 
             LoadResult.Page(
-                data = movies?.results?.map { it ?: TvSeriesReviewDto.Result(null, null, null, null, null, null, null)} ?: listOf(),
+                data = movies?.results?.map { it ?: UserReviewDto.Result(null, null, null, null, null, null, null)} ?: listOf(),
                 prevKey = if (page == 1) null else page.minus(1),
                 nextKey = if (movies?.results?.isEmpty() == true) null else page.plus(1),
             )
