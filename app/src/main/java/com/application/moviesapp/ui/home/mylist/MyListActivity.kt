c6389fb7cc8a88a88bbf9bfbb479f76c1559406f
sheikh-20 <sheikhzs1032@gmail.com>
2024-11-10 21:32:55 +0530

    youtube player aspect ratio fix

---
+++ app/src/main/java/com/application/moviesapp/ui/home/mylist/MyListActivity.kt
@@ -0,0 +1,50 @@
+package com.application.moviesapp.ui.home.mylist
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.coroutineScope
+import com.application.moviesapp.base.BaseActivity
+import com.application.moviesapp.ui.home.NotificationApp
+import com.application.moviesapp.ui.theme.MoviesAppTheme
+import com.application.moviesapp.ui.viewmodel.ProfileViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class MyListActivity: BaseActivity() {
+    companion object {
+        fun startActivity(activity: Activity?) {
+            val intent = Intent(activity, MyListActivity::class.java)
+            activity?.startActivity(intent)
+        }
+    }
+
+    private val profileViewModel: ProfileViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTransparentStatusBar()
+
+        lifecycle.coroutineScope.launch {
+            profileViewModel.isDarkMode.collect {
+                setContent {
+                    MoviesAppTheme(darkTheme = it.data) {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            NotificationApp()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/application/moviesapp/ui/home/mylist/MyListApp.kt
@@ -0,0 +1,4 @@
+package com.application.moviesapp.ui.home.mylist
+
+class MyListApp {
+}
